a9779a03f5067f7f58d8de1844aa994e7df53398
Elder Oliveira <47331781+elderoliver@users.noreply.github.com>
2025-01-06 14:08:25 -0300

    Update date_functions.sql

--- date_functions.sql
+++ date_functions.sql
@@ -1,5 +1,13 @@
 
 getDate()
 
-Convert(DATETIME, '01/31/2025', 101)
+Convert(DATETIME, '01/31/2025', 101); 
+
+-- Converting the date using this mask 'dd/MM/yyyy'
+DECLARE @Date DATE = GETDATE();
+SELECT FORMAT(@Date, 'dd/MM/yyyy') AS FormattedDate;
+
+-- Covnerting the date using the mask code 103 that means 'dd/MM/yyyy'
+DECLARE @Date DATE = GETDATE();
+SELECT CONVERT(VARCHAR(10), @Date, 103) AS FormattedDate;
 
